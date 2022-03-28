6347df19f4c9ea664c2722f9d01ff485dbfa8ad3
Parakh Shah <parakhshah2804@gmail.com>
2022-03-28 15:07:23 +0530

    second commit

--- src/components/CampaignTable.test.js
+++ src/components/CampaignTable.test.js
@@ -1,20 +1,23 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable quotes */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable no-undef */
 import React from "react";
+import { Provider } from "react-redux";
+import { render } from '@testing-library/react';
 import CampaignTable from "./CampaignTable";
 import renderer from "react-test-renderer";
-import { Provider } from "react-redux";
 import store from "../store.js"
 
+describe('Campaign table test',() => {
+  it("First snapshot test", () => {
+    const { asFragment } = render(
+      <Provider store={store}>
+        <CampaignTable />
+      </Provider>
+    );  
+    expect(asFragment()).toMatchSnapshot();
+  
+  });
+});
 
-test("First snapshot test", () => {
-  const component = renderer.create(
-    <Provider store={store}>
-      <CampaignTable />
-    </Provider>
-  );
-  let campaign = component.toJSON();
-
-  expect(campaign).toMatchSnapshot();
-
-})
